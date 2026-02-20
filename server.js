fc2caf592fcce987e6b7a6c008906ce73393c25f
ufcvip1-lgtm <ufcvip1@gmail.com>
2026-02-20 06:35:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("➡️ INCOMING:", req.method, req.url);
+  next();
+});
 
 const TOKEN = process.env.CRYPTO_PAY_TOKEN;
 const API_BASE = process.env.CRYPTO_PAY_API_BASE;
